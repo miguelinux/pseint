188b0591055f07fed1d467288b21364aa7cefb21
zaskar <zaskar_84@yahoo.com.ar>
2012-07-03 15:41:30 -0300

    corregida compilacion de psdraw2 en windows por tercera vez

--- psdraw2/DNassiSchne.cpp
+++ psdraw2/DNassiSchne.cpp
@@ -1,9 +1,9 @@
-#include "Entity.h"
-#include "Global.h"
-#include "Draw.h"
 #include <GL/glut.h>
 #include <iostream>
 #include <cstdlib>
+#include "Entity.h"
+#include "Global.h"
+#include "Draw.h"
 using namespace std;
 
 static void DrawRectangle(const float *color, int x, int y, int wl, int wr, int h) {
