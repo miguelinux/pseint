d2fecf49af86bd987a6ab2b0c87b2d89ce60af0b
zaskar <zaskar@trece>
2018-02-02 15:49:19 -0300

    corregido un error al inicializar la ruta para el indice de recuperacion ante errores, y agregados los fuentes osdep que faltaban de comits anteriores

---
+++ wxPSeInt/osdep.cpp
@@ -0,0 +1,103 @@
+#include "osdep.h"
+
+
+#if defined(__WIN32__) || defined(__WIN64__)
+
+#	include <windows.h>
+#	include <tlhelp32.h>
+#	include <stddef.h>
+#	include <stdlib.h>
+#	include <cstdio>
+
+	void OSDep::AppInit() {
+		HMODULE user32 = LoadLibrary("user32.dll");
+		typedef BOOL (*SetProcessDPIAwareFunc)();
+		SetProcessDPIAwareFunc setDPIAware = (SetProcessDPIAwareFunc)GetProcAddress(user32,"SetProcessDPIAware");
+		if (setDPIAware) setDPIAware();
+		FreeLibrary(user32);
+	}
+
+	static int GetDPI_impl() {
+		HMODULE shcore = LoadLibrary("Shcore.dll");
+		if (!shcore) return 0;
+		typedef HRESULT (*GetDpiForMonitorFunc)(HMONITOR,DWORD,UINT*,UINT*);
+		GetDpiForMonitorFunc GetDpiForMonitor = (GetDpiForMonitorFunc)GetProcAddress(shcore,"GetDpiForMonitor");
+		UINT x=0,y=0;
+		if (GetDpiForMonitor)
+			GetDpiForMonitor( MonitorFromWindow(GetDesktopWindow(),MONITOR_DEFAULTTOPRIMARY),0, &x, &y);
+		FreeLibrary(shcore);
+		return x;
+	}
+
+	int OSDep::GetDPI() {
+		static int x=-1;
+		if (x==-1) { x = GetDPI_impl(); if (x<=0) x=96; }
+		return x;
+	}
+
+#endif
+
+
+
+
+
+
+
+#ifdef __linux__
+	
+#	include <cstdlib>
+#	include <iostream>
+#	include <list>
+#	include <X11/Xlib.h>
+#	include <X11/Xatom.h>
+
+	void OSDep::AppInit() {
+		
+	}
+
+	static int GetDPI_impl() {
+		char *displayname = NULL;
+		Display *dpy = XOpenDisplay (displayname);
+		// 25.4 mm = 1 inch
+		double xres = ((((double) DisplayWidth(dpy,0)) * 25.4) / ((double) DisplayWidthMM(dpy,0)));
+		//	double yres = ((((double) DisplayHeight(dpy,0)) * 25.4) / ((double) DisplayHeightMM(dpy,0)));
+		int x = (int) (xres + 0.5) /*, y = (int) (yres + 0.5)*/;
+		XCloseDisplay (dpy);
+		return x;
+	}
+
+	int OSDep::GetDPI() {
+		static int x=-1;
+		if (x==-1) { x = GetDPI_impl(); if (x<=0) x=96; }
+		return x;
+	}
+
+#endif
+
+
+
+
+
+
+
+
+#ifdef __APPLE__
+
+	// esto es para evitar el problema de no poder hacerle foco a la ventana en Mac sin tener que hacer un application bundle (ver OnInit)
+#	ifdef __WXMAC__
+#		include <ApplicationServices/ApplicationServices.h>
+#	endif
+
+	void OSDep::AppInit() {
+		// esto es para evitar el problema de no poder hacerle foco a la ventana en Mac sin tener que hacer un application bundle
+		ProcessSerialNumber PSN;
+		GetCurrentProcess(&PSN);
+		TransformProcessType(&PSN,kProcessTransformToForegroundApplication); // este es para que pueda recibir el foco
+		SetFrontProcess( &PSN ); // este es para que no aparezca en segundo plano
+	}
+
+	int OSDep::GetDPI() { 
+		return 96; 
+	}
+
+#endif
---
+++ wxPSeInt/osdep.h
@@ -0,0 +1,10 @@
+#ifndef OSDEP_H
+#define OSDEP_H
+
+class OSDep {
+public:
+	static void AppInit();
+	static int GetDPI();
+};
+
+#endif
