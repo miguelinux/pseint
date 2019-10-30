83e54f45d15ba1056838d9ffd4fcd0e363886479
zaskar <zaskar@trece>
2019-10-30 15:22:02 -0300

    correcciones en la generacion del paquete de 64bits para que los binarios encuentren los dylibs de wx

---
+++ dist/fix_dylibs.sh
@@ -0,0 +1,24 @@
+cd $1/Contents || exit 1
+
+LIST="MacOS/pseint Resources/psterm Resources/pseval Resources/psdrawE Resources/psdraw3"
+mkdir -p Frameworks
+
+
+while ! [ "$LIST" = "" ]; do
+	NEXT_LIST=""
+	for FILE in $LIST; do
+
+		echo Processing $FILE...
+		for FLIB in $(x86_64-apple-darwin18-otool -L $FILE | grep /opt/mac | cut -d '(' -f 1); do 
+			NLIB=$(echo $FLIB|rev|cut -d '/' -f 1|rev)
+			echo "    found $NLIB"
+			x86_64-apple-darwin18-install_name_tool -change $FLIB @executable_path/../Frameworks/$NLIB $FILE
+			if ! [ -f Frameworks/$NLIB ]; then 
+				cp $FLIB Frameworks/$NLIB; 
+				NEXT_LIST="$NEXT_LIST Frameworks/$NLIB"
+			fi
+		done
+	
+	done
+	LIST="$NEXT_LIST"
+done
