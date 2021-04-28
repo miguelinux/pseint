21ee27ff3f4aa6c1e4d6fdeff8c16403ee58aeea
zaskar <zaskar@trece>
2021-04-28 09:16:01 -0300

    pseint-20210427

--- imgs/splash.sh
+++ imgs/splash.sh
@@ -6,8 +6,8 @@ inkscape splash.svg --export-id=splash_dark  --export-png=../temp/imgs/splash_da
 
 W=$(file ../bin/imgs/splash_light.png |cut -d ',' -f 2|sed 's/ //g'|cut -d x -f 1)
 H=$(file ../bin/imgs/splash_light.png |cut -d ',' -f 2|sed 's/ //g'|cut -d x -f 2)
-let W=$W-2
-let H=$H-2
+let W=$W-4
+let H=$H-4
 
 convert -crop ${W}x${H}+1+1 ../temp/imgs/splash_light.png ../bin/imgs/splash_light.png
 convert -crop ${W}x${H}+1+1 ../temp/imgs/splash_dark.png  ../bin/imgs/splash_dark.png
